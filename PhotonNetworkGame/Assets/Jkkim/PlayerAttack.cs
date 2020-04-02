8355b2121cda1942ed16a3da4610980dd45e458a
sub3Dreamer <dgyoo200@gmail.com>
2020-04-02 14:31:41 +0900

    [jkkim] Attack RPC 메소드 수정(테스트)

--- PhotonNetworkGame/Assets/Jkkim/PlayerAttack.cs
+++ PhotonNetworkGame/Assets/Jkkim/PlayerAttack.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerAttack : Photon.MonoBehaviour, IPunObservable
 {
+    [SerializeField] PhotonView _photonView;
 
     #region Interface
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
@@ -11,16 +12,28 @@ public class PlayerAttack : Photon.MonoBehaviour, IPunObservable
 
     }
     #endregion
-
+    
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-            OnAttack();
+        if (_photonView.isMine)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                Attack();
+        }
     }
 
-    [PunRPC]
-    void OnAttack()
+    void Attack()
     {
-        CommonDebug.Log("Attack!!!");
+        CommonDebug.Log("Attack!!");
+        string callRpcMethodName = "OnAttack";
+        _photonView.RPC(callRpcMethodName, PhotonTargets.Others);
     }
+
+    #region RPC Event
+    [PunRPC]
+    void OnAttacked()
+    {
+        CommonDebug.Log("OnAttacked!!!");
+    }
+    #endregion
 }
